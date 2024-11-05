353ace69e0c2879cda2082e4b122dc918a4785a1
Álvaro Muñoz Domínguez <163410842+AlvaroMunozDominguez@users.noreply.github.com>
2024-11-05 12:50:21 +0100

    Delete PSP_HLC_04_Grupal directory

--- PSP_HLC_04_Grupal/src/psp_hlc_04_grupal/Exceptions/MostrarDatosListaResumen.java
+++
@@ -1,9 +0,0 @@
-
-package psp_hlc_04_grupal.Exceptions;
-
-public class MostrarDatosListaResumen {
-    /*TO DO: Crear un metodo que recoga todos los elementos de Asignaturas
-      y que calcule la nota media de las asignaturas de un alumno
-    */
-    //Statements y ResultSets simples
-}
